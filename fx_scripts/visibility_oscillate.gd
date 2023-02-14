c26a090bf69fbf3bf7537a5c18a0ef9217dc0b1a
Hardy Whitney <htw6174@gmail.com>
2023-02-14 18:26:40 -0500

    add random firefly size

--- fx_scripts/visibility_oscillate.gd
+++ fx_scripts/visibility_oscillate.gd
@@ -1,8 +1,10 @@
-extends CanvasItem
+extends Node2D
 
 export(float) var max_visibility = 1
 export(float) var min_visibliity = -1
 export(float) var period_seconds = 1.0
+export(float) var scale_min = 0.1
+export(float) var scale_max = 0.3
 
 var random_delay: float
 var start_visibility: float
@@ -10,6 +12,9 @@ var start_visibility: float
 func _ready():
 	random_delay = rand_range(0, period_seconds)
 	start_visibility = self.modulate.a
+	var rand_scale = rand_range(scale_min, scale_max)
+	self.scale.x = rand_scale
+	self.scale.y = rand_scale
 
 func _process(delta):
 	var center = (max_visibility + min_visibliity) / 2.0
